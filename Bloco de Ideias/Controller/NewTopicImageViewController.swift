d29140e4957f0e3f19c344342c7037dbea075a2f
Paloma Bispo <palomabispo@alu.ufc.br>
2018-06-20 14:24:27 -0300

    Add image in topic

--- Bloco de Ideias/Controller/NewTopicImageViewController.swift
+++ Bloco de Ideias/Controller/NewTopicImageViewController.swift
@@ -15,25 +15,33 @@ class NewTopicImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Show ActionSheet
         showAlert()
         
-
-        // Do any additional setup after loading the view.
+        //Set imagepicker delegate to self
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func captureImage(_ sender: UIButton) {
         showAlert()
     }
     
+    @IBAction func saveAction(_ sender: Any) {
+        
+    }
+
+    @IBAction func cancelAction(_ sender: Any) {
+        
+    }
+    
     func showAlert(){
+        //Set details of actionsheet options
         let actionSheet: UIAlertController = UIAlertController(title: "Capture an image", message: "Choose an option", preferredStyle: .actionSheet)
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
         let cameraAction = UIAlertAction(title: "Camera", style: .default)
         { _ in
             guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) else {
@@ -42,37 +50,35 @@ class NewTopicImageViewController: UIViewController {
                 self.present(alert, animated: true, completion: nil)
                 return
             }
-            
             self.imagePicker.sourceType = .camera
-            
             self.present(self.imagePicker, animated: true, completion: nil)
         }
-        
         let galeriaAction = UIAlertAction(title: "Library", style: .default)
         { _ in
             self.imagePicker.sourceType = .photoLibrary
-            
             self.present(self.imagePicker, animated: true, completion: nil)
         }
         
+        //Add options to actionsheet
         actionSheet.addAction(cancelAction)
         actionSheet.addAction(cameraAction)
         actionSheet.addAction(galeriaAction)
         
+        //Show actionsheet
         self.present(actionSheet, animated: true, completion: nil)
     }
-    
-
 }
+
 extension NewTopicImageViewController: UIImagePickerControllerDelegate,  UINavigationControllerDelegate {
+    //Get the selected image and put it in the UIImageView
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             self.image.image = pickedImage
         }
-        
         dismiss(animated: true, completion: nil)
     }
     
+    //Handle cancel button
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
