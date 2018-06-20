946af462dd07fbe7b51ec898cc702dc26966f7e5
LevyCristian <jolevy.cristian@gmail.com>
2018-06-20 17:13:33 -0300

    Add topics in idea

--- Bloco de Ideias/Controller/IdeaViewController.swift
+++ Bloco de Ideias/Controller/IdeaViewController.swift
@@ -19,8 +19,7 @@ class IdeaViewController: UIViewController {
     //Variables
     var indexIdeaSelect = 0
     var idea = Idea()
-    var topicsList : [Idea] = []
-//    var topicsList : [Topic] = []
+    var topicsList : [Topic] = []
     var longPressGR: UILongPressGestureRecognizer!
     var movingIndexPath: NSIndexPath?
     
@@ -34,7 +33,7 @@ class IdeaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Core data
+        //Core data RECEBER OBJETO IDEIA DA TELA ANTERIOR
         let entity = DataManager.getEntity(entity: "Idea")
         let query = DataManager.getAll(entity: entity)
         
@@ -46,20 +45,37 @@ class IdeaViewController: UIViewController {
         }else{
             NSLog("Error on reading ideas from Database...")
         }
+        
+        
+        //Core Data
+        let entityTopic = DataManager.getEntity(entity: "Topic")
+        let topics = DataManager.getAll(entity: entityTopic)
+        if (topics.success){
+            if(topics.objects.count == 0){
+                NSLog("Não existem topicos.")
+            }else{
+                topicsList.removeAll()
+                for top in topics.objects as! [Topic] {
+                    topicsList.append(top)
+                }
+            }
+        }else{
+            NSLog("Erro ao buscar topicos.")
+        }
         collectionView.reloadData()
         
         //Topics Collection View
         let nibText = UINib(nibName: "TopicTextCollectionViewCell", bundle: nil)
-        self.collectionView.register(nibText, forCellWithReuseIdentifier: "TopicCell")
+        self.collectionView.register(nibText, forCellWithReuseIdentifier: "TopicTextCell")
         let nibImage = UINib(nibName: "TopicImageCollectionViewCell", bundle: nil)
-        self.collectionView.register(nibImage, forCellWithReuseIdentifier: "TopicCell")
+        self.collectionView.register(nibImage, forCellWithReuseIdentifier: "TopicImageCell")
         let nibDraw = UINib(nibName: "TopicDrawCollectionViewCell", bundle: nil)
-        self.collectionView.register(nibDraw, forCellWithReuseIdentifier: "TopicCell")
+        self.collectionView.register(nibDraw, forCellWithReuseIdentifier: "TopicDrawCell")
         let nibNew = UINib(nibName: "NewIdeaCollectionViewCell", bundle: nil)
-        self.collectionView.register(nibNew, forCellWithReuseIdentifier: "NewTopicCell")
+        self.collectionView.register(nibNew, forCellWithReuseIdentifier: "NewIdeaCell")
         
-//        self.collectionView.dataSource = self
-//        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         
         //Edit Collection View Long Press
         longPressGR = UILongPressGestureRecognizer(target: self, action: #selector(longPressed(gesture:)))
@@ -200,80 +216,110 @@ extension IdeaViewController : UICollectionViewDelegate, UICollectionViewDataSou
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if(indexPath.row == 0){
-            return self.collectionView.dequeueReusableCell(withReuseIdentifier: "NewTopicCell", for: indexPath) as! NewIdeaCollectionViewCell
+            return self.collectionView.dequeueReusableCell(withReuseIdentifier: "NewIdeaCell", for: indexPath) as! NewIdeaCollectionViewCell
         }else{
             //Use diferent types of cells depending on the topic type
-//            if topicsList[indexPath.row-1].type == .text {
-//                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicCell", for: indexPath) as! TopicTextCollectionViewCell
-//                cell.title.text = topicsList[indexPath.row-1].title
-//                cell.desc.text = topicsList[indexPath.row-1].desc
-//                cell.deleteButton.tag = indexPath.row-1
-//                cell.delegate = self
-//            }
-//            if topicsList[indexPath.row-1].type == .image {
-//                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicCell", for: indexPath) as! TopicImageCollectionViewCell
-//                cell.title.text = topicsList[indexPath.row-1].title
-//                cell.image.image = UIImage(data: topicsList[indexPath.row-1].image!)
-//                cell.deleteButton.tag = indexPath.row-1
-//                cell.delegate = self
-//            }
-//            if topicsList[indexPath.row-1].type == .draw {
-//                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicCell", for: indexPath) as! TopicDrawCollectionViewCell
-//                cell.title.text = topicsList[indexPath.row-1].title
-//                cell.image.image = UIImage(data: topicsList[indexPath.row-1].image!)
-//                cell.deleteButton.tag = indexPath.row-1
-//                cell.delegate = self
-//            }
-
-//            if isEditing {
-//                cell.startWiggling()
-//            } else {
-//                cell.stopWiggling()
-//            }
-//
-//            if (indexPath as NSIndexPath) == movingIndexPath {
-//                cell.alpha = 0.7
-//                cell.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-//            } else {
-//                cell.alpha = 1.0
-//                cell.transform = CGAffineTransform.identity
-//            }
-
-            let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicCell", for: indexPath) as! TopicTextCollectionViewCell
-            return cell
+            if topicsList[indexPath.row-1].typeT == TopicsEnum.text.rawValue {
+                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicTextCell", for: indexPath) as! TopicTextCollectionViewCell
+                cell.title.text = topicsList[indexPath.row-1].titleT
+                cell.desc.text = topicsList[indexPath.row-1].descT
+                cell.deleteButton.tag = indexPath.row-1
+                //cell.delegate = self
+                
+                if isEditing {
+                    cell.startWiggling()
+                } else {
+                    cell.stopWiggling()
+                }
+                
+                if (indexPath as NSIndexPath) == movingIndexPath {
+                    cell.alpha = 0.7
+                    cell.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+                } else {
+                    cell.alpha = 1.0
+                    cell.transform = CGAffineTransform.identity
+                }
+                return cell
+            } else if topicsList[indexPath.row-1].typeT == TopicsEnum.image.rawValue {
+                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicImageCell", for: indexPath) as! TopicImageCollectionViewCell
+                cell.title.text = topicsList[indexPath.row-1].titleT
+                cell.image.image = UIImage(data: topicsList[indexPath.row-1].imageT! as Data)
+                cell.deleteButton.tag = indexPath.row-1
+                //cell.delegate = self
+                
+                if isEditing {
+                    cell.startWiggling()
+                } else {
+                    cell.stopWiggling()
+                }
+                
+                if (indexPath as NSIndexPath) == movingIndexPath {
+                    cell.alpha = 0.7
+                    cell.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+                } else {
+                    cell.alpha = 1.0
+                    cell.transform = CGAffineTransform.identity
+                }
+                return cell
+            } else if topicsList[indexPath.row-1].typeT == TopicsEnum.draw.rawValue {
+                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicDrawCell", for: indexPath) as! TopicDrawCollectionViewCell
+                cell.title.text = topicsList[indexPath.row-1].titleT
+                cell.image.image = UIImage(data: topicsList[indexPath.row-1].imageT! as Data)
+                cell.deleteButton.tag = indexPath.row-1
+                //cell.delegate = self
+                
+                if isEditing {
+                    cell.startWiggling()
+                } else {
+                    cell.stopWiggling()
+                }
+                
+                if (indexPath as NSIndexPath) == movingIndexPath {
+                    cell.alpha = 0.7
+                    cell.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+                } else {
+                    cell.alpha = 1.0
+                    cell.transform = CGAffineTransform.identity
+                }
+                return cell
+            } else {
+                let cell2 = self.collectionView.dequeueReusableCell(withReuseIdentifier: "TopicCell", for: indexPath) as! TopicTextCollectionViewCell
+                return cell2
+            }
         }
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        if (indexPath.row == 0){
-//            //Set details of actionsheet options
-//            let actionSheet: UIAlertController = UIAlertController(title: "Add topic", message: "Choose an option", preferredStyle: .actionSheet)
-//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//            let textAction = UIAlertAction(title: "Text", style: .default)
-//            { _ in
-//                self.performSegue(withIdentifier: "newTopicText", sender: nil)
-//            }
-//            let drawAction = UIAlertAction(title: "Draw", style: .default)
-//            { _ in
-//                self.performSegue(withIdentifier: "newTopicDraw", sender: nil)
-//            }
-//            let galeryAction = UIAlertAction(title: "Image", style: .default)
-//            { _ in
-//                self.performSegue(withIdentifier: "newTopicImage", sender: nil)
-//            }
-//            
-//            //Add options to actionsheet
-//            actionSheet.addAction(cancelAction)
-//            actionSheet.addAction(textAction)
-//            actionSheet.addAction(drawAction)
-//            actionSheet.addAction(galeryAction)
-//            
-//            //Show actionsheet
-//            self.present(actionSheet, animated: true, completion: nil)
-//        }else{
+        if (indexPath.row == 0){
+            //Set details of actionsheet options
+            let actionSheet: UIAlertController = UIAlertController(title: "Add topic", message: "Choose an option", preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let textAction = UIAlertAction(title: "Text", style: .default)
+            { _ in
+                self.performSegue(withIdentifier: "newTopicText", sender: nil)
+            }
+            let drawAction = UIAlertAction(title: "Draw", style: .default)
+            { _ in
+                self.performSegue(withIdentifier: "newTopicDraw", sender: nil)
+            }
+            let galeryAction = UIAlertAction(title: "Image", style: .default)
+            { _ in
+                self.performSegue(withIdentifier: "newTopicImage", sender: nil)
+            }
+            
+            //Add options to actionsheet
+            actionSheet.addAction(cancelAction)
+            actionSheet.addAction(textAction)
+            actionSheet.addAction(drawAction)
+            actionSheet.addAction(galeryAction)
+            
+            //Show actionsheet
+            self.present(actionSheet, animated: true, completion: nil)
+        }else{
 //            self.ideaSelected = indexPath.row - 1
 //            performSegue(withIdentifier: "viewIdea", sender: nil)
-//        }
+            print("Click in a topic cell")
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
