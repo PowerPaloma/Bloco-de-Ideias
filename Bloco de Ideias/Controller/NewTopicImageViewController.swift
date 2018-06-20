0d05c110ee7ef0e8ec01d64b866b8bfcb5c1873f
LevyCristian <jolevy.cristian@gmail.com>
2018-06-20 12:07:37 -0300

    novo topico image controller

---
+++ Bloco de Ideias/Controller/NewTopicImageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  NewTopicImageViewController.swift
+//  Bloco de Ideias
+//
+//  Created by Ada 2018 on 20/06/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class NewTopicImageViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showAlert()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func captureImage(_ sender: UIButton) {
+        showAlert()
+    }
+    
+    func showAlert(){
+        let actionSheet: UIAlertController = UIAlertController(title: "Capture an image", message: "Choose an option", preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default)
+        { _ in
+            guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) else {
+                let alert: UIAlertController = UIAlertController(title: "Oops...", message: "Camera is not available", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            self.imagePicker.sourceType = .camera
+            
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        let galeriaAction = UIAlertAction(title: "Library", style: .default)
+        { _ in
+            self.imagePicker.sourceType = .photoLibrary
+            
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        actionSheet.addAction(cancelAction)
+        actionSheet.addAction(cameraAction)
+        actionSheet.addAction(galeriaAction)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+
+}
+extension NewTopicImageViewController: UIImagePickerControllerDelegate,  UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.image.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
