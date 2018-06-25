d656dd0baeff60fc94742632e6cdfe6df4c48ade
LevyCristian <jolevy.cristian@gmail.com>
2018-06-25 19:11:48 -0300

    new swipe

---
+++ Bloco de Ideias/update/OverlayPresentationController.swift
@@ -0,0 +1,47 @@
+//
+//  OverlayPresentationController.swift
+//  Bloco de Ideias
+//
+//  Created by Ada 2018 on 25/06/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class OverlayPresentationController: UIPresentationController {
+    let dimmingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        return view
+    }()
+    
+    override func presentationTransitionWillBegin() {
+        dimmingView.frame = containerView!.bounds
+        dimmingView.alpha = 0.0
+        containerView!.insertSubview(dimmingView, at: 0)
+        
+        presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 1.0
+        })
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 0.0
+        }) { _ in
+            self.dimmingView.removeFromSuperview()
+        }
+    }
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        var size = containerView!.bounds.size
+        size.height = size.height - 40
+        return CGRect(origin: CGPoint(x: 0, y: 40), size: size)
+    }
+    
+    override func containerViewWillLayoutSubviews() {
+        dimmingView.frame = containerView!.bounds
+        presentedView?.frame = frameOfPresentedViewInContainerView
+    }
+}
+
