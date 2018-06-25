1709df9143c94c74203b828284a97e5edc6b5b0f
Paloma Bispo <palomabispo@alu.ufc.br>
2018-06-25 08:12:07 -0300

    details

--- Bloco de Ideias/Controller/ImproveIdeaViewController.swift
+++ Bloco de Ideias/Controller/ImproveIdeaViewController.swift
@@ -18,26 +18,48 @@ class ImproveIdeaViewController: UIViewController {
     
     //variables
     var idea = Idea()
+    var suggestionsOrder: [(Suggestion,Int64)] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tagsOfIdea = idea.tags
+        let tagsNames = idea.tags!.map{ ($0 as! Tag).name! }
         let context = DataManager.getContext()
         
-        //Core Data - getting sugguestions who has the tag of this idea
+        //Core Data - getting suggestions who has the tag of this idea
         do {
             let fetchRequest : NSFetchRequest<Suggestion> = Suggestion.fetchRequest()
-            fetchRequest.predicate = NSPredicate(format: "ANY tags.name in %@", tagsOfIdea!)
+            //AND (suggestionStatus.@count == 0 OR NOT (suggestionStatus.idea.title CONTAINS[cd] %@))
+            fetchRequest.predicate = NSPredicate(format: "(ANY tags.name in %@) AND (%@ in processes.name)",
+                                                 tagsNames //,idea.title!
+                                                    ,idea.process!.name!
+            )
+            
             let fetchedResults = try context.fetch(fetchRequest)
-            if let aContact = fetchedResults.first {
-                print("DEU CERTO")
+            if fetchedResults.count > 0 {
+                for sug in fetchedResults {
+                    print(sug.titleS!)
+                    
+                    let fetchRequest2 : NSFetchRequest<SuggestionOrder> = SuggestionOrder.fetchRequest()
+                    fetchRequest2.predicate = NSPredicate(format: "process.name == %@ AND suggestion.titleS == %@",
+                                                         idea.process!.name!, sug.titleS!)
+                    
+                    let fetchedResults2 = try context.fetch(fetchRequest2)
+                    if fetchedResults2.count > 0 {
+                        print(fetchedResults2.count)
+                        
+                        suggestionsOrder.append((sug, fetchedResults2.first!.order))
+                    }
+                }
             }
         }
         catch {
             print ("fetch task failed", error)
         }
-       
+        
+        self.titleSugguestion.text = suggestionsOrder[0].0.titleS
+        self.descSuggestion.text = suggestionsOrder[0].0.descS
+//
     }
 
     override func didReceiveMemoryWarning() {
